00310d2c8d366d6ebb526336a38a17109d6dd07a
Manas <mm74894n@pace.edu>
2025-02-23 14:15:49 -0500

    updated

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const { createPost, getPosts } = require('../controllers/postController');
+const { createPost, getPosts, likePost } = require('../controllers/postController');
 
 const router = express.Router();
 
 // Route to create a new post
 router.post('/add', createPost);
 
-// Route to fetch all posts
+// Route to fetch all posts (or filtered by query parameter)
 router.get('/all', getPosts);
+
+// NEW: Route to like a specific post
+router.put('/like/:postId', likePost);
 
 module.exports = router;
