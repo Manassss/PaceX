efbbd033fb021862fe019fb5a667b9da29779ed5
“SidharthRajKhandelwal” <sk50149n@pace.edu>
2025-04-22 15:55:33 -0400

    merge with main

--- client/src/App.js
+++ client/src/App.js
@@ -24,9 +24,9 @@ import CommunityDetail from "./components/CommunityDetail";
 import SearchPanel from "./components/Search";
 //import { host } from '../components/apinfo';
 // ✅ Initialize socket connection
-const host = process.env.NODE_ENV === 'development'
-  ? process.env.REACT_APP_DEV_API_BASE_URL
-  : process.env.REACT_APP_PROD_API_BASE_URL;
+const host = process.env.REACT_APP_PROD_API_BASE_URL
+// ? process.env.REACT_APP_DEV_API_BASE_URL
+// : process.env.REACT_APP_PROD_API_BASE_URL;
 const socket = io(`${host}`, { transports: ["websocket"] });
 
 function AppContent({ userId }) {
